b46299fee9eabc27aed905546ed8caf0edcb3fce
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-01 00:17:35 -0700

    Add heapify up

--- stanford/djikstra/djikstra.py
+++ stanford/djikstra/djikstra.py
@@ -1,4 +1,3 @@
-import os
 from vertex import Vertex, Edge
 from math import inf
 
@@ -7,14 +6,10 @@ file_name = "dijkstraData.txt"
 class Dijkstra(object):
     def __init__(self, file_path):
         self.vertices = {}
-        self.edges = {}
         self.convert_adj_list_to_graph(file_path)
 
     def get_vertices(self):
         return self.vertices
-
-    def get_edges(self):
-        return self.edges
 
     def find_shortest_distance(self, start_vertex_key, end_vertex_key):
         if start_vertex_key == end_vertex_key:
@@ -64,10 +59,6 @@ class Dijkstra(object):
                     if key not in self.vertices.keys():
                         self.vertices[key] = Vertex(key)
                     new_edge = Edge(self.vertices[start_key], self.vertices[key], int(el[1]))
-                    if key not in self.edges.keys():
-                        self.edges[key] = [new_edge]
-                    else:
-                        self.edges[key].append(new_edge)
                     self.vertices[start_key].add_out_edge(new_edge)
 
 
---
+++ stanford/djikstra/heap.py
@@ -0,0 +1,50 @@
+class MinHeap(object):
+    def __init__(self):
+        self.store = []
+
+    def insert(self, node):
+        self.store.append(node)
+        self.heapify_up()
+
+    def extract_min(self):
+        self.store[0], self.store[-1] = self.store[-1], self.store[0]
+        min_node = self.store.pop()
+        self.heapify_down()
+
+    def heapify_up(self):
+        new_node_idx = len(self.store) - 1
+        parent_idx = (new_node_idx - 1) / 2
+        while new_idx_idx != 0 and \
+            self.store[new_node_idx].get_value() < self.store[parent_idx].get_value():
+            self.store[new_node_idx], self.store[parent_idx] = \
+            self.store[parent_idx], self.store[new_node_idx]
+            new_node_idx = parent_idx
+            parent_idx = (new_node_idx - 1) / 2
+
+    def heapify_down(self):
+        current_node_idx = 0
+        swap = true
+        while swap:
+            swap = false
+            children = children_indeces(current_node_idx)
+            for child in children:
+                
+
+
+    def children_indeces(self, parent_idx):
+        result = []
+        length = len(self.store)
+        left = (2 * parent_idx) + 1
+        right = (2 * parent_idx) + 2
+        if left < length:
+            result.append(left)
+            if right < length:
+                result.append(right)
+        return result
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
