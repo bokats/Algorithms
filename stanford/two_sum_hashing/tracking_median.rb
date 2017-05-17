9435aadc67deb25f1cac88e1dd02548dbfca4c73
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-16 17:46:42 -0700

    Add tracking median

---
+++ stanford/two_sum_hashing/tracking_median.rb
@@ -0,0 +1,216 @@
+class Node
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+end
+
+class MinHeap
+
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def insert(node)
+    @store << node
+    heapify_up
+  end
+
+  def extract
+    @store[0], @store[@store.length - 1] =
+      @store[@store.length - 1], @store[0]
+    min_node = @store.pop
+    heapify_down
+    min_node
+  end
+
+  def peek
+    @store[0]
+  end
+
+  def heapify_up
+    current_idx = @store.length - 1
+    parent_idx = parent(current_idx)
+
+    while !parent_idx.nil?
+
+      if @store[current_idx].value < @store[parent_idx].value
+        @store[current_idx], @store[parent_idx] =
+          @store[parent_idx], @store[current_idx]
+        current_idx = parent_idx
+        parent_idx = parent(current_idx)
+      else
+        break
+      end
+    end
+  end
+
+  def heapify_down
+    current_idx = 0
+    swap = true
+    while swap
+      children_idx = children(current_idx)
+      lowest_index = current_idx
+      swap = false
+      children_idx.each do |child_idx|
+        if @store[child_idx].value < @store[lowest_index].value
+          lowest_index = child_idx
+          swap = true
+        end
+      end
+
+      if swap
+        @store[current_idx], @store[lowest_index] = @store[lowest_index], @store[current_idx]
+        current_idx = lowest_index
+      end
+    end
+  end
+
+  def parent(index)
+    return (index - 1) / 2 if index > 0
+    nil
+  end
+
+  def children(index)
+    result = []
+    first_child = (index * 2) + 1
+    second_child = (index * 2) + 2
+    result << first_child if first_child < @store.length
+    result << second_child if second_child < @store.length
+    result
+  end
+end
+
+class MaxHeap
+
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def insert(node)
+    @store << node
+    heapify_up
+  end
+
+  def extract
+    @store[0], @store[@store.length - 1] =
+      @store[@store.length - 1], @store[0]
+    min_node = @store.pop
+    heapify_down
+    min_node
+  end
+
+  def peek
+    @store[0]
+  end
+
+  def heapify_up
+    current_idx = @store.length - 1
+    parent_idx = parent(current_idx)
+
+    while !parent_idx.nil?
+
+      if @store[current_idx].value > @store[parent_idx].value
+        @store[current_idx], @store[parent_idx] =
+          @store[parent_idx], @store[current_idx]
+        current_idx = parent_idx
+        parent_idx = parent(current_idx)
+      else
+        break
+      end
+    end
+  end
+
+  def heapify_down
+    current_idx = 0
+    swap = true
+    while swap
+      children_idx = children(current_idx)
+      highest_index = current_idx
+      swap = false
+      children_idx.each do |child_idx|
+        if @store[child_idx].value > @store[highest_index].value
+          highest_index = child_idx
+          swap = true
+        end
+      end
+
+      if swap
+        @store[current_idx], @store[highest_index] = @store[highest_index], @store[current_idx]
+        current_idx = highest_index
+      end
+    end
+  end
+
+  def parent(index)
+    return (index - 1) / 2 if index > 0
+    nil
+  end
+
+  def children(index)
+    result = []
+    first_child = (index * 2) + 1
+    second_child = (index * 2) + 2
+    result << first_child if first_child < @store.length
+    result << second_child if second_child < @store.length
+    result
+  end
+end
+
+class TrackMedian
+  attr_reader :min_heap, :max_heap, :median
+
+  def initialize
+    @min_heap = MinHeap.new
+    @max_heap = MaxHeap.new
+    @min_heap_length = 0
+    @max_heap_length = 0
+    @median = nil
+  end
+
+  def insert(number)
+    new_node = Node.new(number)
+    if @max_heap_length == 0
+      @max_heap.insert(new_node)
+      @max_heap_length += 1
+      @median = number.to_f
+    elsif number < @median
+      @max_heap.insert(new_node)
+      @max_heap_length += 1
+    else
+      @min_heap.insert(new_node)
+      @min_heap_length += 1
+    end
+    calculate_median
+  end
+
+  def calculate_median
+
+    size_difference = @min_heap_length - @max_heap_length
+    if size_difference > 1
+      @max_heap.insert(@min_heap.extract)
+      @median = (@max_heap.peek.value.to_f + @min_heap.peek.value) / 2
+      @min_heap_length -= 1
+      @max_heap_length += 1
+    elsif size_difference < -1
+      @min_heap.insert(@max_heap.extract)
+      @median = (@max_heap.peek.value.to_f + @min_heap.peek.value) / 2
+      @max_heap_length -= 1
+      @min_heap_length += 1
+    elsif size_difference == 1
+      @median = @min_heap.peek.value.to_f
+    elsif size_difference == -1
+      @median = @max_heap.peek.value.to_f
+    else
+      @median = (@max_heap.peek.value.to_f + @min_heap.peek.value) / 2
+    end
+    @median
+  end
+end
+
+t = TrackMedian.new
--- stanford/two_sum_hashing/two_sum.py
+++ stanford/two_sum_hashing/two_sum.py
@@ -1,10 +1,44 @@
-def read_file(filename):
-    array = []
-    file = open(filename, 'r')
-    for line in file:
-        array.append(int(line))
-    return array
+class TwoSum(object):
+    def __init__(self, array = None):
+        if array:
+            self.array = array
+        else:
+            self.array = []
+        self.set_nums = set([])
 
-print(read_file('algo1-programming_prob-2sum.txt')[0])
+    def read_file(self, filename):
+        file = open(filename, 'r')
+        for line in file:
+            self.array.append(int(line))
+        self.set_nums = set(self.array)
 
-# def two_sum(array)
+    def find_two_sum(self):
+        result = 0
+
+        for t in range(-5, 2):
+            print(t)
+            if self.has_two_sum(t):
+                result += 1
+        return result
+
+    def has_two_sum(self, t):
+        seen_nums = set([])
+        for num in self.set_nums:
+            if t - num in seen_nums:
+                self.set_nums.remove(num)
+                self.set_nums.remove(t - num)
+                return True
+            else:
+                seen_nums.add(num)
+        return False
+
+t = TwoSum()
+t.read_file('algo1-programming_prob-2sum.txt')
+# print(t.find_two_sum())
+
+def has_two_sum(n, num_set):
+    print(n)
+    return any(((n-x) in num_set) and 2*x != n for x in num_set)
+num_set = t.set_nums
+two_sum = sum(has_two_sum(n, num_set) for n in range(-10000, 10001))
+print(two_sum)
