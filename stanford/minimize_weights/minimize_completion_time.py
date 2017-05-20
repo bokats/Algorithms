fdd667dc0e7081d92173a7246af513c77a74f4bd
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-20 13:49:21 -0700

    Add min spanning trees'

--- stanford/minimize_weights/minimize_completion_time.py
+++ stanford/minimize_weights/minimize_completion_time.py
@@ -13,10 +13,32 @@ class MinimizeCompletionTime(object):
         for task in self.data:
             task.append(task[0] - task[1])
 
-        self.data = sorted(self.data, key=lambda x: (x[2], x[0]), reverse=True)
-        print(self.data)
+        current_length = 0
+        weighted_avg_time = 0
+        for task in sorted(self.data, key=lambda x: (x[2], x[0]), reverse=True):
+            current_length += task[1]
+            weighted_avg_time += task[0] * current_length
+
+        return weighted_avg_time
+
+    def minimize_by_ratio(self):
+        for task in self.data:
+            task.append(task[0] / task[1])
+
+        current_length = 0
+        weighted_avg_time = 0
+        for task in sorted(self.data, key=lambda x: x[2], reverse=True):
+            current_length += task[1]
+            weighted_avg_time += task[0] * current_length
+
+        return weighted_avg_time
 
 
-m = MinimizeCompletionTime()
-m.read_file('data.txt')
-m.minimize_by_difference()
+
+# m = MinimizeCompletionTime()
+# m.read_file('data.txt')
+# print(m.minimize_by_difference())
+#
+# m = MinimizeCompletionTime()
+# m.read_file('data.txt')
+# print(m.minimize_by_ratio())
