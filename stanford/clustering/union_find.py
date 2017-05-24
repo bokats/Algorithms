8f0f37645033f5da38165d0dca9744bfbe53fd66
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-24 15:18:04 -0700

    Complete union find

--- stanford/clustering/union_find.py
+++ stanford/clustering/union_find.py
@@ -14,6 +14,16 @@ class UnionFind(object):
         self.vertices = {}
         self.leaders = {}
 
+    def add_vertex(self, vertex, leader = None):
+        if leader:
+            vertex.leader = leader
+            self.leaders[leader.key].append(vertex.key)
+        elif vertex.leader.key in self.leaders.keys():
+            self.leaders[vertex.leader.key].append(vertex.key)
+        else:
+            self.leaders[vertex.leader.key] = [vertex.key]
+        self.vertices[vertex.key] = vertex
+
     def find(self,vertex_key):
         return self.vertices[vertex_key].leader
 
@@ -22,7 +32,7 @@ class UnionFind(object):
             merge_components(leader_one_key, leader_two_key)
         else:
             merge_components(leader_two_key, leader_one_key)
-        
+
     def merge_components(self, larger_leader_key, smaller_leader_key):
         for key in self.leaders[smaller_leader_key]:
             self.vertices[key].leader = self.vertices[larger_leader_key]
