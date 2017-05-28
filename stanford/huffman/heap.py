2a7852a5f7fb63175849cf7d7f68b5ca837572fc
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-28 16:39:25 -0700

    Add binary assignment

---
+++ stanford/huffman/heap.py
@@ -0,0 +1,51 @@
+class MinHeap(object):
+    def __init__(self):
+        self.store = []
+
+    def insert(self, node):
+        self.store.append(node)
+        self.heapify_up()
+
+    def extract_min(self):
+        self.store[0], self.store[-1] = self.store[-1], self.store[0]
+        min_node = self.store.pop()
+        self.heapify_down()
+        return min_node
+
+    def heapify_up(self):
+        new_node_idx = len(self.store) - 1
+        parent_idx = int((new_node_idx - 1) / 2)
+        while new_node_idx != 0 and \
+            self.store[new_node_idx].value < self.store[parent_idx].value:
+            self.store[new_node_idx], self.store[parent_idx] = \
+            self.store[parent_idx], self.store[new_node_idx]
+            new_node_idx = parent_idx
+            parent_idx = int((new_node_idx - 1) / 2)
+
+    def heapify_down(self):
+        current_node_idx = 0
+        swap = True
+        while swap:
+            swap = False
+            children = self.children_indeces(current_node_idx)
+            swap_idx = current_node_idx
+            for child_idx in children:
+                if self.store[child_idx].value < self.store[swap_idx].value:
+                    swap_idx = child_idx
+                    swap = True
+            if swap:
+                self.store[current_node_idx], self.store[swap_idx] = \
+                self.store[swap_idx], self.store[current_node_idx]
+                current_node_idx = swap_idx
+        return self.store
+
+    def children_indeces(self, parent_idx):
+        result = []
+        length = len(self.store)
+        left = (2 * parent_idx) + 1
+        right = (2 * parent_idx) + 2
+        if left < length:
+            result.append(left)
+            if right < length:
+                result.append(right)
+        return result
--- stanford/huffman/huffman.py
+++ stanford/huffman/huffman.py
@@ -1,7 +1,53 @@
+from heap import MinHeap
+
 class Node(object):
-    def __init__(self,key,left,right):
+    def __init__(self,key,value,left,right):
         self.key = key
+        self.value = value
         self.left = left
         self.right = right
+        self.binary = None
 
 class Huffman(object):
+    def __init__(self):
+        self.heap = MinHeap()
+
+    def read_file(self, filename):
+        file = open(filename, 'r')
+        count = 0
+        for line in file:
+            if count != 0:
+                line = int(line)
+                node = Node(str(count),line, None, None)
+                self.heap.insert(node)
+                count += 1
+
+    def build_tree(self):
+
+        while len(self.heap.store) > 1:
+            min_two = []
+            for _ in range(2):
+                min_two.append(self.heap.extract_min())
+            self.merge_nodes(min_two[0], min_two[1])
+
+        self.assign_binary(self.heap.extract_min())
+
+    def merge_nodes(self, node1, node2):
+        new_node = Node(node1.key + node2.key, node1.value + node2.value, node1, node2)
+        self.heap.insert(new_node)
+
+    def assign_binary(self, root):
+        stack = [root]
+        while len(stack) > 0:
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+                current_node.right.binary = 1
+            if current_node.left:
+                stack.append(current_node.left)
+                current_node.left.binary = 0
+
+        
+h = Huffman()
+h.read_file('huffman.txt')
+h.build_tree()
