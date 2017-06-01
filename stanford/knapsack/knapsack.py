1a23e4f97eefbbf08e89bdb608d2c715d70d2326
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-31 22:46:54 -0700

    Find items

--- stanford/knapsack/knapsack.py
+++ stanford/knapsack/knapsack.py
@@ -3,7 +3,9 @@ import numpy as np
 class Knapsack(object):
     def __init__(self):
         self.items = None
-        self.weight = 0
+        self.max_weight = 0
+        self.values = {}
+        self.grid = None
 
     def read_file(self,filename):
         file = open(filename, 'r')
@@ -12,7 +14,7 @@ class Knapsack(object):
         for line in file:
             line = line.split()
             if first_line:
-                self.weight = int(line[0])
+                self.max_weight = int(line[0])
                 self.items = np.zeros((int(line[1]), 2), int)
                 first_line = False
             else:
@@ -21,26 +23,59 @@ class Knapsack(object):
                 count += 1
 
     def find_optimal(self):
-        grid = np.zeros((len(self.items) + 1, self.weight + 1), int)
+        self.grid = np.zeros((len(self.items) + 1, self.max_weight + 1), int)
 
-        for item_idx in range(1, len(grid)):
+        for item_idx in range(1, len(self.grid)):
             item = self.items[item_idx - 1]
-            for weight in range(len(grid[item_idx])):
+            for weight in range(len(self.grid[item_idx])):
                 if weight < item[1]:
-                    grid[item_idx][weight] = grid[item_idx - 1][weight]
+                    self.grid[item_idx][weight] = self.grid[item_idx - 1][weight]
                 else:
-                    grid[item_idx][weight] = max(item[0] + grid[item_idx - 1][weight - item[1]], grid[item_idx - 1][weight])
+                    self.grid[item_idx][weight] = max(item[0] + self.grid[item_idx - 1][weight - item[1]], self.grid[item_idx - 1][weight])
 
-        return grid[len(grid) - 1][len(grid[0]) - 1]
+        return self.grid[len(self.grid) - 1][len(self.grid[0]) - 1]
 
     def find_optimal_fast(self):
-        prev_row = np.zeros(self.weight + 1, int)
+        for weight in range(self.max_weight + 1):
+            self.values[weight] = [0,0]
+
+        for item_idx in range(len(self.items)):
+            print(item_idx)
+            item = self.items[item_idx]
+            if not self.max_weight < item[1]:
+                if self.values[self.max_weight - item[1]][1] < item_idx:
+                    self.find_table_values(self.max_weight - item[1], item_idx)
+                self.values[self.max_weight][0] = max(item[0] + self.values[self.max_weight - item[1]][0], self.values[self.max_weight][0])
+            # import pdb; pdb.set_trace()
+        return self.values[self.max_weight][0]
+
+    def find_choses_items(self):
+
+        item_idx = len(self.items)
+        weight = self.weight
+        taken_items = []
+        while item_idx > 0:
+            if self.grid[item_idx][weight] > self.grid[item_idx - 1][weight]:
 
 
+            ]
 
-        return new_row[-1]
+    def find_table_values(self, weight, item_idx):
+
+        while self.values[weight][1] < item_idx:
+            item = self.items[self.values[weight][1]]
+            # import pdb; pdb.set_trace()
+            if not weight < item[1]:
+                if self.values[weight - item[1]][1] < self.values[weight][1]:
+                    # import pdb; pdb.set_trace()
+                    self.find_table_values(weight - item[1], self.values[weight][1])
+                self.values[weight][0] = max(item[0] + self.values[weight - item[1]][0], self.values[weight][0])
+            self.values[weight][1] += 1
+            # print(self.values[weight])
 
 k = Knapsack()
-k.read_file('knapsack_big.txt')
-# print(k.find_optimal())
-print(k.find_optimal_fast())
+# k.read_file('knapsack_big.txt')
+# k.read_file('knapsack1.txt')
+k.read_file('test2.txt')
+print(k.find_optimal())
+# print(k.find_optimal_fast())
