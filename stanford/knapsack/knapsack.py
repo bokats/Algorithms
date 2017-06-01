825be05cb2dcca71f352f4c1dfa495576fe020b6
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-31 18:25:50 -0700

    Add knapsack with only two arays

--- stanford/knapsack/knapsack.py
+++ stanford/knapsack/knapsack.py
@@ -2,19 +2,23 @@ import numpy as np
 
 class Knapsack(object):
     def __init__(self):
-        self.items = []
+        self.items = None
         self.weight = 0
 
     def read_file(self,filename):
         file = open(filename, 'r')
         first_line = True
+        count = 0
         for line in file:
             line = line.split()
             if first_line:
                 self.weight = int(line[0])
+                self.items = np.zeros((int(line[1]), 2), int)
                 first_line = False
             else:
-                self.items.append([int(line[0]), int(line[1])])
+                self.items[count][0] = int(line[0])
+                self.items[count][1] = int(line[1])
+                count += 1
 
     def find_optimal(self):
         grid = np.zeros((len(self.items) + 1, self.weight + 1), int)
@@ -29,6 +33,25 @@ class Knapsack(object):
 
         return grid[len(grid) - 1][len(grid[0]) - 1]
 
+    def find_optimal_fast(self):
+        prev_row = np.zeros(self.weight + 1, int)
+
+        item_idx = 0
+        while item_idx < len(self.items):
+            print(item_idx)
+            item = self.items[item_idx]
+            new_row = np.zeros(self.weight + 1, int)
+            for weight in range(len(new_row)):
+                if weight < item[1]:
+                    new_row[weight] = prev_row[weight]
+                else:
+                    new_row[weight] = max(item[0] + prev_row[weight - item[1]], prev_row[weight])
+            prev_row = new_row
+            item_idx += 1
+
+        return new_row[-1]
+
 k = Knapsack()
-k.read_file('knapsack1.txt')
-print(k.find_optimal())
+k.read_file('knapsack_big.txt')
+# print(k.find_optimal())
+print(k.find_optimal_fast())
