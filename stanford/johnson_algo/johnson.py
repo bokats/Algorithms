fbe56cb435be90f1b8e11e4e9adbf95f71f6eaba
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-06-08 11:46:28 -0700

    Debug numpy arrays

--- stanford/johnson_algo/johnson.py
+++ stanford/johnson_algo/johnson.py
@@ -15,16 +15,29 @@ class BellmanFord(object):
 
     def read_file(self,filename):
         file = open(filename, 'r')
-
+        count = 0
         for line in file:
             line = line.split()
             for i in range(len(line)):
                 line[i] = int(line[i])
-            count = 0
             if len(line) < 3:
+
                 self.edges = np.zeros((line[1], 3), int)
-                self.distances = np.zeros(line[0] + 1, int)
+                self.distances = np.zeros(line[0] + 1, float)
+
             else:
                 self.edges[count] = line
-                if count in self.in_edges.keys():
-                    
+                if line[1] in self.in_edges.keys():
+                    self.in_edges[line[1]] = np.append(self.in_edges[line[1]], [count])
+                else:
+                    self.in_edges[line[1]] = np.array([count], int)
+                count += 1
+
+        for i in range(len(self.distances)):
+            self.distances[i] = math.inf
+
+        print(self.edges[47977])
+        print(count)
+
+bf = BellmanFord()
+bf.read_file('g1.txt')
