768f4c3168b965d7e274d43b4f81bcdd01883e83
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-06-19 20:05:06 -0700

    Get dp solution

--- stanford/tsp/tsp.py
+++ stanford/tsp/tsp.py
@@ -30,51 +30,40 @@ class TSP(object):
         for i in range(1, self.number_of_cities + 1):
             for j in range(1, self.number_of_cities + 1):
                 self.distances[i][j] = self.calculate_distance(self.coordinates[i], self.coordinates[j])
-
         results = {}
-        for m in range(2, self.number_of_cities):
+        results[tuple([1])] = np.zeros(self.number_of_cities + 1)
+        for m in range(2, self.number_of_cities + 1):
+            print(m)
             for C in itertools.combinations(range(1, self.number_of_cities + 1), m):
+                if 1 not in C:
+                    continue
                 for j in C:
                     if j == 1:
-                        results[tuple(C)] = np.zeros(len(C))
+                        results[C] = np.full(self.number_of_cities + 1, np.inf)
                     else:
+                        minimum = np.inf
                         for k in C:
-                            minimum = np.inf
                             if k != j:
-                                temp = C[:]
+                                temp = list(C)
                                 temp.remove(j)
-                                if results[temp][k] + self.distances[k][j] < minimum:
-                                    minumum = results[temp][k] + self.distances[k][j]
+                                if results[tuple(temp)][k] + self.distances[k][j] < minimum:
+                                    minimum = results[tuple(temp)][k] + self.distances[k][j]
                         results[tuple(C)][j] = minimum
-        
 
+        shortest_dis = np.inf
+        for j in range(2, self.number_of_cities + 1):
+            score = self.distances[j][1] + results[tuple(range(1, self.number_of_cities + 1))][j]
+            if score < shortest_dis:
+                shortest_dis = score
 
+        return shortest_dis
 
-        # A = {(frozenset([0, idx+1]), idx+1): (dist, [0,idx+1]) for idx,dist in enumerate(self.distances[0][1:])}
-        # for m in range(2, self.number_of_cities):
-        #     print(m)
-        #     B = {}
-        #     for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, self.number_of_cities), m)]:
-        #         for j in S - {0}:
-        #             B[(S, j)] = min( [(A[(S-{j},k)][0] + self.distances[k][j], A[(S-{j},k)][1] + [j]) for k in S if k != 0 and k!=j])  #this will use 0th index of tuple for ordering, the same as if key=itemgetter(0) used
-        #     A = B
-        # res = min([(A[d][0] + self.distances[0][d[1]], A[d][1]) for d in iter(A)])
-        # return res
-
-
-    # def solve_tsp_dynamic(self):
-    #     all_distances = [[length(x,y) for y in points] for x in points]
-    #     #initial value - just distance from 0 to every other point + keep the track of edges
-    #     A = {(frozenset([0, idx+1]), idx+1): (dist, [0,idx+1]) for idx,dist in enumerate(all_distances[0][1:])}
-    #     cnt = len(points)
-    #     for m in range(2, cnt):
-    #         B = {}
-    #         for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, cnt), m)]:
-    #             for j in S - {0}:
-    #                 B[(S, j)] = min( [(A[(S-{j},k)][0] + all_distances[k][j], A[(S-{j},k)][1] + [j]) for k in S if k != 0 and k!=j])  #this will use 0th index of tuple for ordering, the same as if key=itemgetter(0) used
-    #         A = B
-    #     res = min([(A[d][0] + all_distances[0][d[1]], A[d][1]) for d in iter(A)])
-    #     return res[1]
+    def find_distance(self,res):
+        total = 0
+        for i in range(len(res) - 1):
+            j = i + 1
+            total += self.distances[res[i]][res[j]]
+        return total
 
 tsp = TSP()
 tsp.read_file('test1.txt')
